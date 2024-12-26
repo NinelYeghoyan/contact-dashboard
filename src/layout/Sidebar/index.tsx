b6bab9e3126c3fe9c6c97f20627ed07a9feb76e5
Ninel Yeghoyan <ninel.yeghoyan@gmail.com>
2024-12-26 13:37:09 +0400

    fix: change the import method

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -1,10 +1,8 @@
-import Button from '@components/Button';
-import Input from '@components/Input';
+import { Button, Input } from '@components/index.ts';
+import { UsersList } from '@features/user';
 import type { ChangeEvent, FC } from 'react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
-import UsersList from '../../features/user/components/List';
 
 const Sidebar: FC = () => {
   const { t } = useTranslation();
